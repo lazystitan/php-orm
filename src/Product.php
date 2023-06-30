e66bd2ae49cba73b42b798266ac700b6c4cf04d6
riton <riton@timez.com>
2023-06-30 16:01:16 +0800

    init

---
+++ src/Product.php
@@ -0,0 +1,58 @@
+<?php
+// src/Product.php
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'products')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    /** @var Collection<int, Product> */
+    private Collection $products;
+
+    public function assignToProduct(Product $product): void
+    {
+        $this->products[] = $product;
+    }
+
+    /** @return Collection<int, Product> */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
+}
